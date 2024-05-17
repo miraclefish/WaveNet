70c879ec25bd7ee416cd237d40621ca47aa3961c
yyy96 <yuyifei@sjtu.edu.cn>
2024-05-17 14:34:57 +0800

    base configuration when run script

---
+++ base_config.py
@@ -0,0 +1,7 @@
+# Description: This file contains the base configuration for the project.
+
+# Current Workspace
+WORK_SPACE = '/root/autodl-pub/YYF/WaveNet'
+
+# Montage path for eeg data loading from EDF files.
+MONTAGE_PATH = '/root/autodl-pub/YYF/WaveNet/data/DOCS'
